81bbc083bf1bf1d95d7aea60fbc0494bf7cee145
Sterling Hanenkamp <hanenkamp@cpan.org>
2011-12-01 22:16:02 -0600

    Cleaning up some of the details
    * Textbox is single liner
    * Show the search button on the soft keybaord
    * Enter causes search
    * Use the light theme

--- src/com/qubling/sidekick/SidekickActivity.java
+++ src/com/qubling/sidekick/SidekickActivity.java
@@ -3,7 +3,9 @@ package com.qubling.sidekick;
 import com.qubling.sidekick.metacpan.ModuleSearch;
 
 import android.app.Activity;
+import android.inputmethodservice.Keyboard.Key;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,11 +22,25 @@ public class SidekickActivity extends Activity {
         final EditText queryText = (EditText) findViewById(R.id.text_search);
         final ListView resultsView = (ListView) findViewById(R.id.list_search_results);
         
-        ImageButton searchButton = (ImageButton) findViewById(R.id.button_search);
+        final ImageButton searchButton = (ImageButton) findViewById(R.id.button_search);
         searchButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View searchButton) {
 				new ModuleSearch(resultsView, queryText.getText().toString()).execute();
+			}
+		});
+        
+        queryText.setOnKeyListener(new View.OnKeyListener() {
+			
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				switch (keyCode) {
+				case KeyEvent.KEYCODE_ENTER:
+				case KeyEvent.KEYCODE_NUMPAD_ENTER:
+					searchButton.performClick();
+					return true;
+				}
+				return false;
 			}
 		});
     }
