738c4a3d80cf269ca0bbd32ce49820ccfc7d4568
Sterling Hanenkamp <hanenkamp@cpan.org>
2011-12-11 22:12:11 -0600

    Fixed the drop shadows a bit

--- src/com/qubling/sidekick/ModuleViewActivity.java
+++ src/com/qubling/sidekick/ModuleViewActivity.java
@@ -6,9 +6,13 @@ import com.qubling.sidekick.widget.ModuleHelper;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class ModuleViewActivity extends Activity {
 	public static final String EXTRA_MODULE = "com.qubling.sidekick.intent.extra.MODULE";
@@ -30,6 +34,19 @@ public class ModuleViewActivity extends Activity {
 		setTitle(module.getModuleName());
 		
 		WebView podView = (WebView) findViewById(R.id.module_pod);
+		
+		podView.setWebViewClient(new WebViewClient() {
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView podView, String url) {
+				if (url.startsWith(MetaCPANSearch.METACPAN_API_URL)) {
+					return false;
+				}
+				
+				Toast.makeText(ModuleViewActivity.this, "Not Yet Implemented", Toast.LENGTH_SHORT).show();
+				return true;
+			}
+		});
+		
 		podView.loadUrl(MetaCPANSearch.METACPAN_API_URL + "/pod/" + module.getModuleName());
 	}
 }
