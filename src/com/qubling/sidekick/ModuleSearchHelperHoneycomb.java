0abda10134c2da43b2499c9c705eb91051505974
Sterling Hanenkamp <hanenkamp@cpan.org>
2012-02-22 21:12:08 -0600

    First round implementation using the SearchView widget
    ...sucks because each search starts the activity over again. This is not
    necessary.

--- src/com/qubling/sidekick/ModuleSearchHelperHoneycomb.java
+++ src/com/qubling/sidekick/ModuleSearchHelperHoneycomb.java
@@ -1,6 +1,10 @@
 package com.qubling.sidekick;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.SearchView;
 
 public class ModuleSearchHelperHoneycomb extends ModuleSearchHelper {
@@ -12,13 +16,17 @@ public class ModuleSearchHelperHoneycomb extends ModuleSearchHelper {
     public boolean onCreateOptionsMenu(Menu menu) {
     	super.onCreateOptionsMenu(menu);
     	
-    	SearchView moduleSearchView = (SearchView) menu.findItem(R.id.menu_module_search).getActionView();
+    	SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_module_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+        searchView.setIconifiedByDefault(false);
     	
     	return true;
     }
     
     @Override
     public boolean onSearchRequested() {
+    	// TODO Make onSearchRequested() do something useful.
     	return false;
     }
 }
